d1d0e1c74de82abe74ed4f5a6a567ab326c241c0
angela-fra-uas <85959823+angela-fra-uas@users.noreply.github.com>
2022-11-24 16:46:54 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,122 @@
+const container = document.querySelector(".container");
+
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  );
+  container.innerHTML = output;
+};
+
+if ('localStorage' in window || 'sessionStorage' in window) {
+  var selectedEngine;
+
+  var logTarget = document.getElementById('target');
+  var valueInput = document.getElementById('value');
+
+  var reloadInputValue = function () {
+  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
+    valueInput.value = window[selectedEngine].getItem('myKey') || '';
+  }
+  
+  var selectEngine = function (engine) {
+    selectedEngine = engine;
+    reloadInputValue();
+  };
+
+  function handleChange(change) {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
+    logTarget.appendChild(newState);
+  }
+
+  var radios = document.querySelectorAll('#selectEngine input');
+  for (var i = 0; i < radios.length; ++i) {
+    radios[i].addEventListener('change', function () {
+      selectEngine(this.value)
+    });
+  }
+  
+  selectEngine('localStorage');
+
+  valueInput.addEventListener('keyup', function () {
+    window[selectedEngine].setItem('myKey', this.value);
+  });
+
+  var onStorageChanged = function (change) {
+    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
+    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
+    if (engine === selectedEngine) {
+      reloadInputValue();
+    }
+  }
+
+  window.addEventListener('storage', onStorageChanged);
+
+function getReadFile(reader, i) {
+  return function () {
+    var li = document.querySelector('[data-idx="' + i + '"]');
+
+    li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
+  }
+}
+
+function readFiles(files) {
+  document.getElementById('count').innerHTML = files.length;
+
+  var target = document.getElementById('target');
+  target.innerHTML = '';
+
+  for (var i = 0; i < files.length; ++i) {
+    var item = document.createElement('li');
+    item.setAttribute('data-idx', i);
+    var file = files[i];
+
+    var reader = new FileReader();
+    reader.addEventListener('load', getReadFile(reader, i));
+    reader.readAsText(file);
+
+    item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
+    target.appendChild(item);
+  };
+}
+
+async function writeFile() {
+  if (!window.chooseFileSystemEntries) {
+    alert('Native File System API not supported');
+    return;
+  }
+  
+  const target = document.getElementById('target');
+  target.innerHTML = 'Opening file handle...';
+  
+  const handle = await window.chooseFileSystemEntries({
+    type: 'save-file',
+  });
+  
+  const file = await handle.getFile()
+  const writer = await handle.createWriter();
+  await writer.write(0, 'Hello world from What Web Can Do!');
+  await writer.close()
+  
+  target.innerHTML = 'Test content written to ' + file.name + '.';
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
